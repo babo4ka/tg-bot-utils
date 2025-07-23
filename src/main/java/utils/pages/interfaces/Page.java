3f9bf489b7863ef9498786f87ffc4c89cf6c5f5e
basra4ka <evgeha_ivanov_01@bk.ru>
2025-07-23 21:36:41 +0300

    changed return value of Page methods

--- src/main/java/utils/pages/interfaces/Page.java
+++ src/main/java/utils/pages/interfaces/Page.java
@@ -4,24 +4,25 @@ import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import utils.tuples.Pair;
 
 import java.util.List;
 
 public interface Page {
 
-    default List<PartialBotApiMethod<Message>> execute(Update update) throws TelegramApiException {
+    default List<Pair<PartialBotApiMethod<Message>, Boolean>> execute(Update update) throws TelegramApiException {
         return null;
     }
 
-    default List<PartialBotApiMethod<Message>> executeWithArgs(Update update, String...args){
+    default List<Pair<PartialBotApiMethod<Message>, Boolean>> executeWithArgs(Update update, String...args){
         return null;
     }
 
-    default List<PartialBotApiMethod<Message>> executeCallback(Update update) throws TelegramApiException {
+    default List<Pair<PartialBotApiMethod<Message>, Boolean>> executeCallback(Update update) throws TelegramApiException {
         return null;
     }
 
-    default List<PartialBotApiMethod<Message>> executeCallbackWithArgs(Update update, String...args){
+    default List<Pair<PartialBotApiMethod<Message>, Boolean>> executeCallbackWithArgs(Update update, String...args){
         return null;
     }
 }
